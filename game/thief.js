a9d08fe53e8d4752123fdebc5f52a100f56f9bb4
Youssef Awad <deadpackets@protonmail.com>
2017-04-25 17:23:59 +0400

    Update thief.js

--- game/thief.js
+++ game/thief.js
@@ -1,1 +1,31 @@
-
+//Global array of websites
+    var VISITED_WEBSITES = [];
+    $('.box').click(function(e) {
+      e.preventDefault()
+      var test = $(this).attr("selected")
+      if (test) {
+        $(this).removeAttr("style")
+        $(this).removeAttr("selected")
+        var i = VISITED_WEBSITES.indexOf(this.href)
+        VISITED_WEBSITES.splice(i, 1)
+      } else {
+        $(this).css("background-color", "green")
+        $(this).attr("selected", "yes")
+        $(this).css("content", "url(https://image.flaticon.com/icons/png/128/23/23400.png)")
+        VISITED_WEBSITES.push(this.href)
+      }
+    })
+    $('.submit').click(function() {
+      $('#main_game').hide()
+      $('.result').show()
+      VISITED_WEBSITES.forEach(function(data, index) {
+        var html = '<p>' + data + '</p>'
+        $('.websites').append(html)
+        console.log("You have visited " + data + " before!")
+      })
+    })
+    $('.start').click(function() {
+      $('.modal').hide()
+      $('#welcome').hide()
+      $('#main_game').show()
+    })
